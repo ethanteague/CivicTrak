c583ed13dd044676449ae28c30eca782110b8a0d
Ethan Teague <metrowebz@gmail.com>
2025-07-19 15:58:41 -0400

    Update config

--- docroot/sites/default/settings.platformsh.php
+++ docroot/sites/default/settings.platformsh.php
@@ -5,6 +5,7 @@
  * Platform.sh settings.
  */
 
+use Drupal\redis\Cache\PhpRedis;
 use Platformsh\ConfigReader\Config;
 
 if (file_exists(__DIR__ . '/settings.platformsh.generated.php')) {
@@ -15,14 +16,17 @@ if (getenv('PLATFORM_RELATIONSHIPS') && class_exists(Config::class)) {
   $plat_config = new Config();
 
   // Redis configuration.
-  if ($plat_config->hasRelationship('redis') && extension_loaded('redis')) {
-    $redis = $plat_config->credentials('redis');
+  if ($plat_config->hasRelationship('redis') && extension_loaded('redis') && class_exists(PhpRedis::class)) {
+    $redis = $plat_config->credentials('redis')[0];
 
     $settings['redis.connection']['interface'] = 'PhpRedis';
     $settings['redis.connection']['host'] = $redis['host'];
     $settings['redis.connection']['port'] = $redis['port'];
 
+    // Only override default cache if redis module is installed.
     $settings['cache']['default'] = 'cache.backend.redis';
+
+    // Optional: improve performance of other bins.
     $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
     $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
     $settings['cache']['bins']['config'] = 'cache.backend.chainedfast';
