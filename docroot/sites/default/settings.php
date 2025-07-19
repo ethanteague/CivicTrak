45f0b5862eb5b68ee894953dcb494d68ab478e62
Ethan Teague <metrowebz@gmail.com>
2025-07-19 16:54:45 -0400

    Update config

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -880,10 +880,10 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
   include __DIR__ . '/settings.ddev.php';
 }
 
- if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
-   include $app_root . '/' . $site_path . '/settings.platformsh.php';
- }
+if (file_exists(__DIR__ . '/settings.platformsh.php')) {
+  include __DIR__ . '/settings.platformsh.php';
+}
 
- if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-   include $app_root . '/' . $site_path . '/settings.local.php';
- }
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
